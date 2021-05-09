04754d70a3c53f6e5bc204d023b1bd780ae504db
Nikshitha16 <83922988+Nikshitha16@users.noreply.github.com>
2021-05-10 03:24:48 +0530

    Create footer.php

---
+++ non-pages/footer.php
@@ -0,0 +1,24 @@
+  <footer>
+        <div class="footer">
+            <style>
+                .fab{
+                    font-size:21px;
+                }
+            </style>
+            <center>
+                <a href="https://www.facebook.com/dropsuite/" target="_blank"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;
+                    <a href="https://twitter.com/dropsuite?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter" target="_blank"></i></a>&nbsp;&nbsp;
+                    <a href="https://github.com/LoGaming/Dropsuit-Command-Gmod/pulls"><i class="fab fa-github" target="_blank"></i></a>&nbsp;&nbsp;
+                    <a href="https://www.youtube.com/watch?v=Vc7uBN20pp0" target="_blank"><i class="fab fa-youtube"></i></a>&nbsp;&nbsp;
+                    <a href="https://www.linkedin.com/company/dropsuite/?originalSubdomain=sg"><i class="fab fa-linkedin-in" target="_blank"></i></a><br>
+                <span class="copyright">All copyrights reserved © Dropsuit 2021
+                </span>
+            </center>
+        </div>
+    </footer>
+    <script src="../repository/js/jquery.min.js" type="text/javascript"></script>
+    <script src="../repository/js/main.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="../repository/js/bootstrap.min.js" type="text/javascript"></script>
+  </body>
+</html>
